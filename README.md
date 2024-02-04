3627a71cb68f5cdf88529ad4fcf1654d2b089ad4
Matt D. Correa <matiasdylanc@gmail.com>
2024-02-04 16:34:22 -0300

    Cambios bot

---
+++ README.md
@@ -0,0 +1,3 @@
+Check it out
+
+https://core.telegram.org/bots/api
--- bot.py
+++ bot.py
@@ -43,3 +43,95 @@ def listener(messages): # Con esto, estamos definiendo una función llamada 'lis
 
 
 bot.set_update_listener(listener) # Así, le decimos al bot que utilice como función escuchadora nuestra función 'listener' declarada arriba.
+
+
+# PARTE 2 // AYUDA
+
+@bot.message_handler(commands=['ayuda']) # Indicamos que lo siguiente va a controlar el comando '/ayuda'
+
+def command_ayuda(m): # Definimos una función que resuleva lo que necesitemos.
+
+    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
+
+    # se ejecute, la ayuda que hemos definido encima del listener anteriormente. Si por ejemplo no 
+    # quisiéramos hacer flood de mensajes en un grupo podemos cambiar el cid por:
+    # uid = m.from_user.id
+    # Con esto lo que haremos es que cuando pongamos /ayuda se nos envié la información a nuestro chat privado
+    # hay que iniciar por privado primeramente el bot.
+    # bot.send_message( uid, AYUDA)
+    # bot.send_message( cid, «Se te ha enviado la información por privado»)
+    # De esta manera aparte de enviar la información por privado, lo notificaríamos por el grupo.
+
+    bot.send_chat_action(cid, 'typing') # Enviando ...
+
+    time.sleep(1) #La respuesta del bot tarda 1 segundo en ejecutarse
+
+    bot.send_message( cid, AYUDA) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+
+
+
+# PARTE 3 // GRUPO
+
+
+@bot.message_handler(commands=['info']) # Indicamos que lo siguiente va a controlar el comando '/info'
+
+def command_info(m): # Definimos una función que resuleva lo que necesitemos.
+
+    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
+
+    if cid == GRUPO:
+
+        
+
+            bot.send_message( GRUPO, 'mensaje A') # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+
+    else :
+
+            bot.send_message( cid, 'mensaje B')
+
+
+# CONTENIDO MULTIMEDIA
+
+@bot.message_handler(commands=['piensa3D']) # Indicamos que lo siguiente va a controlar el comando '/piensa3D'
+ 
+def command_piensa3D(m): # Definimos una función que resuleva lo que necesitemos.
+ 
+    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
+ 
+    Piensa3D = open('piensa3D.jpg', 'rb')
+ 
+    xxx=open('xxx.mp3', 'rb')
+ 
+    yyy=open('yyy.mp4', 'rb')
+ 
+    zzz=open('zzz.jpg', 'rb')
+ 
+    bot.send_sticker(cid, Piensa3D) # Con la función 'send_sticker()' del bot, enviamos al ID almacenado el texto que queremos.
+ 
+    bot.send_audio(cid, xxx)
+ 
+    bot.send_video(cid, yyy)
+ 
+    bot.send_photo(cid, zzz)
+
+    # bot.send_sticker(cid, zzz)
+ 
+    bot.send_message(cid, "hola")
+ 
+    piensa3D.close()
+ 
+    xxx.close()
+ 
+    yyy.close()
+ 
+    zzz.close()            
+
+
+
+ @bot.message_handler(func=lambda message: message.text == "hola")
+
+def command_text_hola(m):
+
+    time.sleep(1)
+
+    bot.send_message(m.chat.id, "Hola a ti tambien")   
